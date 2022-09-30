9e96e4e4839b4c4c41bdfe0cd8d5ff67e7b32507
edwardyule <nowhyman@hotmail.com>
2022-09-30 19:08:23 +0800

    细节

--- utils/utils.js
+++ utils/utils.js
@@ -7,6 +7,7 @@ function getIdCard() {
   const birthDay = getBirthDay();
   const randomCode = new Array(3).fill(0).map(e => Math.floor(Math.random() * 10)).join('');
   let idCard = `${areaCode}${birthDay}${randomCode}`;
+  console.log(idCard);
   const validCode = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2].map((e, i) => e * idCard[i]).reduce((acu, e) => acu + e, 0);
   return `${idCard}${validCode}`;
 }
@@ -15,7 +16,7 @@ function getBirthDay() {
   const offset = Math.floor(Math.random() * 171);
   const date = new Date(1000 * 60 * 60 * 24 * 365 * (-70 + offset));
   const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-  return `${year}${month}${day}`;
+  return `${year}${month >= 10 ? month : `0${month}`}${day >= 10 ? day : `0${day}`}`;
 }
 
 module.exports = {
